cd7d8a91e3577a74a3d2fcd7865fb79112132627
JarssS8 <adriancgs@gmail.com>
2024-04-08 14:24:01 +0200

    Initial commit with functional web page

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Larrys Car Shop';
+export const SITE_IMAGE = 'https://media.discordapp.net/attachments/1221903748344058028/1221928158362603621/COMPRAVENTA-LOGO.png?ex=661d96c7&is=660b21c7&hm=44eafc94dc022ed67fba247b4188d9cf91bd80038742cdb759d712a0b31feeca&=&format=webp&quality=lossless';
+export const CARS_URL = 'https://gtav-api-production.up.railway.app/api/cars-on-sale';
